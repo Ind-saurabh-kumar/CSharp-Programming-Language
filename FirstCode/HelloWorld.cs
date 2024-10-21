a6f77c37eedfdf9ca6df388732c7feaf4e7c8e9d
Saurabh Kumar <saurabhkumar.conn@gmail.com>
2024-10-21 20:40:18 +0530

    Hello world

---
+++ FirstCode/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+class HelloWorld{
+    public static void Main(String[] args ){
+        
+        // It is for new print in new line
+        Console.WriteLine("Hello World!!!");
+
+        Console.Write("Hello");
+        Console.Write("World!!!");
+        // It is for new print in new line
+        Console.WriteLine("Hello World!!!");
+
+    }
+}
